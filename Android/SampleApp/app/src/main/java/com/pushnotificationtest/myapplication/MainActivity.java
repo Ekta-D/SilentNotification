7511b34ce4a2875635d99287b9b47134737bdab2
Ekta <ekta.dushanj@revmedia.my>
2022-03-11 00:17:50 +0530

    token code changed

--- Android/SampleApp/app/src/main/java/com/pushnotificationtest/myapplication/MainActivity.java
+++ Android/SampleApp/app/src/main/java/com/pushnotificationtest/myapplication/MainActivity.java
@@ -29,24 +29,24 @@ public class MainActivity extends AppCompatActivity {
         Log.d("deviceId",android_id);
 
         /* code to generate app token */
-//        FirebaseMessaging.getInstance().getToken()
-//                .addOnCompleteListener(new OnCompleteListener<String>() {
-//                    @Override
-//                    public void onComplete(@NonNull Task<String> task) {
-//                        if (!task.isSuccessful()) {
-//                            Log.w("TAG", "Fetching FCM registration token failed", task.getException());
-//                            return;
-//                        }
-//
-//                        // Get new FCM registration token
-//                        String token = task.getResult();
-//
-//                        Log.i("Token: ", token);
-//                        // Log and toast
-////                        String msg = getString(R.string.msg_token_fmt, token);
-////                        Log.d(TAG, msg);
-////                        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
-//                    }
-//                });
+        FirebaseMessaging.getInstance().getToken()
+                .addOnCompleteListener(new OnCompleteListener<String>() {
+                    @Override
+                    public void onComplete(@NonNull Task<String> task) {
+                        if (!task.isSuccessful()) {
+                            Log.w("TAG", "Fetching FCM registration token failed", task.getException());
+                            return;
+                        }
+
+                        // Get new FCM registration token
+                        String token = task.getResult();
+
+                        Log.i("Token: ", token);
+                        // Log and toast
+//                        String msg = getString(R.string.msg_token_fmt, token);
+//                        Log.d(TAG, msg);
+//                        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
